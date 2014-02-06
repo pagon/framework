8f8825e3fee204bdd878fcede6a08612af984f8b
hfcorriez <hfcorriez@gmail.com>
2014-02-06 21:40:37 +0800

    Update core pack

--- pack/pagon.core.php
+++ pack/pagon.core.php
@@ -1268,7 +1268,7 @@ class App extends EventEmitter
 
        
         $this->injectors =
-            (!is_array($config) ? Config::load((string)$config) : $config)
+            (!is_array($config) ? Config::load((string)$config)->get() : $config)
             + (!empty($this->injectors['cli']) ? array('buffer' => false) : array())
             + $this->injectors;
 
